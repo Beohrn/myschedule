37fd02eaca11957fcbc0440a6f19fd7bce81378a
japrogBogdan <bb.japrog@gmail.com>
2016-09-11 17:51:12 +0300

    add comments

---
+++ app/src/main/java/com/shedule/zyx/myshedule/utils/Constants.kt
@@ -0,0 +1,13 @@
+package app.voter.xyz.utils
+
+/**
+ * Created by bogdan on 23.08.16.
+ */
+class Constants {
+  companion object {
+    val MATCHES = "matches"
+    val QUIZ = "quizes"
+    val COMMENTS = "comments"
+    val REPLIES = "replies"
+  }
+}
